e548dca9c5d838fce41df6cb3e189fceeb724eda
Frederik Berlaen <frederik@typemytype.com>
2019-10-30 16:55:48 +0100

    flavor is not a method...

--- Batch.roboFontExt/lib/webFormats/woff2Builder.py
+++ Batch.roboFontExt/lib/webFormats/woff2Builder.py
@@ -3,5 +3,5 @@ from fontTools.ttLib import TTFont
 
 def WOFF2Builder(sourcePath, destinationPath):
     font = TTFont(sourcePath)
-    font.flavor("woff2")
+    font.flavor = "woff2"
     font.save(destinationPath)
--- Batch.roboFontExt/lib/webFormats/woffBuilder.py
+++ Batch.roboFontExt/lib/webFormats/woffBuilder.py
@@ -3,5 +3,5 @@ from fontTools.ttLib import TTFont
 
 def WOFFBuilder(sourcePath, destinationPath):
     font = TTFont(sourcePath)
-    font.flavor("woff")
+    font.flavor = "woff"
     font.save(destinationPath)
